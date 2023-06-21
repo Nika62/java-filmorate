cf64ffb731bddc042a637190613a4625fab2b016
Nika <nika@w-up.ru>
2023-06-21 13:41:42 +0300

    Style

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmDbStorage.java
@@ -10,7 +10,6 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.RequestDataBaseException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.sql.Date;
